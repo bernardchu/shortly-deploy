c510a44810488a7944f02f2c7e5258aca51b3c66
Bernard Chu <bernard.chu@yahoo.com>
2014-11-28 12:21:21 -0800

    Added concat and uglify tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,12 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      dist: {
+        src: [
+          'public/client/**/*.js'
+        ],
+        dest:'public/dist/<%= pkg.name %>.js'
+      }
     },
 
     mochaTest: {
@@ -21,6 +27,10 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      build: {
+        src: 'public/dist/<%= pkg.name %>.js',
+        dest: 'public/dist/<%= pkg.name %>.min.js'
+      }
     },
 
     jshint: {
@@ -94,6 +104,8 @@ module.exports = function(grunt) {
   ]);
 
   grunt.registerTask('build', [
+    'concat',
+    'uglify'
   ]);
 
   grunt.registerTask('upload', function(n) {
